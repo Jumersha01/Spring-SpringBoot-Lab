ceb68798ff36eb64a445f73abecf2485636de95c
Jumersha01 <www.junaid555fakir@gmail.com>
2026-05-07 10:31:19 +0530

    Strategy DP using Spring Boot

---
+++ Spring-Boot/PaymentStrategyApp/src/main/java/com/nt/comp/PaymentStrategy.java
@@ -0,0 +1,6 @@
+package com.nt.comp;
+
+public interface PaymentStrategy {
+  void pay(double amount);
+  
+}
---
+++ Spring-Boot/PaymentStrategyApp/src/main/java/com/nt/sbeans/CardPayment.java
@@ -0,0 +1,15 @@
+package com.nt.sbeans;
+
+import org.springframework.stereotype.Component;
+
+import com.nt.comp.PaymentStrategy;
+@Component
+public  final class CardPayment implements PaymentStrategy {
+
+	@Override
+	public void pay(double amount) {
+		System.out.println(" Payment  done  using  card "+amount);
+
+	}
+
+}
