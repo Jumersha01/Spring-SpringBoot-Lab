9033a5677005c16a177fda0dfbd790a7269c35c1
JumershO1 <junaid555fakir@gmail.com>
2026-06-23 19:45:32 +0530

    Untracked codes

---
+++ Spring-Boot/BootProj22_Employee_Resume_Management_System/src/main/java/com/nt/BootProj22EmployeeResumeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj22EmployeeResumeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj22EmployeeResumeManagementSystemApplication.class, args);
+	}
+
+}
